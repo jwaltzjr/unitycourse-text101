8dcd87a68b22d07f76f13664277e17c59d9e1377
jwaltzjr <jwaltzjr@hotmail.com>
2017-02-23 18:50:38 -0600

    Add state and second scene

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -7,29 +7,84 @@ public class TextController : MonoBehaviour
 
     public Text text;
 
+    enum State {Start, Prison};
+    enum Choice : byte {Choice1, Choice2, Choice3, Choice4};
+
+    State player_state;
+    Choice player_choice;
+
 	// Use this for initialization
 	void Start()
     {
-
+        player_state = State.Start;
 	}
 
 	// Update is called once per frame
 	void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        switch (player_state)
         {
-            text.text =
-                "You and your wife, Catelyn, are on a trip to the city " +
-                "to set up shop in the market. It's been a rough year, " +
-                "and the last of your wares are with you in your cart. " +
-                "As you're traveling along the past through the forest " +
-                "that lies between your home and the city, you come upon " +
-                "a camp that seems to be empty. As soon as you've past " +
-                "it men start rushing out from behind the trees.\n\n" +
-                "1. Fight them\n" +
-                "2. Talk to them\n" +
-                "3. Ignore them";
+            case State.Start:
+                StartScene();
+                break;
+            case State.Prison:
+                PrisonScene();
+                break;
         }
 	}
+
+    void LogPlayer()
+    {
+        print(player_state);
+        print(player_choice);
+    }
+
+    void StartScene()
+    {
+        LogPlayer();
+        text.text =
+            "You and your wife, Catelyn, are on a trip to the city " +
+            "to set up shop in the market. It's been a rough year, " +
+            "and the last of your wares are with you in your cart. " +
+            "As you're traveling along the path through the forest " +
+            "that lies between your home and the city, you come upon " +
+            "a camp that seems to be empty. It's quiet, but after you " +
+            "pass men start rushing out from behind the trees.\n\n" +
+            "1. Fight them\n" +
+            "2. Talk to them\n" +
+            "3. Ignore them";
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            player_choice = Choice.Choice1;
+            player_state = State.Prison;
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            player_choice = Choice.Choice2;
+            player_state = State.Prison;
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            player_choice = Choice.Choice3;
+            player_state = State.Prison;
+        }
+    }
+
+    void PrisonScene()
+    {
+        LogPlayer();
+        text.text =
+            "Prison Scene Text\n\n" +
+            "1.\n" +
+            "2.\n" +
+            "3.";
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            player_choice = Choice.Choice1;
+            player_state = State.Start;
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            player_choice = Choice.Choice2;
+            player_state = State.Start;
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            player_choice = Choice.Choice3;
+            player_state = State.Start;
+        }
+    }
 
 }
