61f764974bfc10c78ee22b5f1c66b988dfc93f99
jwaltzjr <jwaltzjr@hotmail.com>
2017-02-23 18:06:21 -0600

    Add first story

--- Assets/Scripts/TextController.cs
+++ Assets/Scripts/TextController.cs
@@ -10,7 +10,7 @@ public class TextController : MonoBehaviour
 	// Use this for initialization
 	void Start()
     {
-        text.text = "Hello world!";
+
 	}
 
 	// Update is called once per frame
@@ -18,7 +18,17 @@ public class TextController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            text.text = "Space key pressed.";
+            text.text =
+                "You and your wife, Catelyn, are on a trip to the city " +
+                "to set up shop in the market. It's been a rough year, " +
+                "and the last of your wares are with you in your cart. " +
+                "As you're traveling along the past through the forest " +
+                "that lies between your home and the city, you come upon " +
+                "a camp that seems to be empty. As soon as you've past " +
+                "it men start rushing out from behind the trees.\n\n" +
+                "1. Fight them\n" +
+                "2. Talk to them\n" +
+                "3. Ignore them";
         }
 	}
 
